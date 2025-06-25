d2b69ebe9ec5f8cf32708ecd35593db5326a0228
BenjaminDestefanis <benjaminisraelb@gmail.com>
2025-06-25 09:33:21 -0300

    backend: app- modelos - rutas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+const courseRoutes = require('./routes/courseRoutes');
 
 // Middlewares
 app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/api/auth', authRoutes);
 app.use(express.json());
 
 // Rutas básicas (las definiremos después)
--- backend/src/models/db.js
+++ backend/src/models/db.js
@@ -1,4 +1,6 @@
 const { Pool } = require('pg');
+
+// Creacion de conexion con la base de datos
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -6,4 +8,12 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
+//Verificacion de conexcion
+
+pool.query('SELECT NOW()')
+  .then(() => console.log("✅ Conexion exitosa con la base de datos : " + process.env.DB_NAME));
+  .catch(error = console.error('❌ Error de conexión:', err));
+
+// se exporta para poder ser utilizado por otros archivos
 module.exports = pool;
