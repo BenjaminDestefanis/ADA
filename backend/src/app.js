6af07cd4fc6e2bfeab6622dc4dbb21ff4deaabcc
BenjaminDestefanis <benjaminisraelb@gmail.com>
2025-06-26 10:15:31 -0300

    app.js backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const pool = require('./database/connection')
 const authRoutes = require('./routes/authRoutes');
 const courseRoutes = require('./routes/courseRoutes');
+
+
+// Solo verifica conexión, NO crea tablas
+pool.query('SELECT NOW()')
+  .then(() => {
+    console.log('✅ DB conectada')
+      if ( process.env.NODE_ENV === 'development') {
+        require('./database/migrations/run-migrations')
+      }
+  })
+
+  .catch(err => {
+    console.error('❌ Error de conexión:', err);
+    process.exit(1); // Detiene la app si no hay DB
+  });
 
 // Middlewares
 app.use(cors());
@@ -16,5 +32,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('API de Udemy Clone');
 });
+
+
+app.listen(3000, () => {
+  console.log('APP lista en http://localhost:3000')
+})
 
 module.exports = app;
